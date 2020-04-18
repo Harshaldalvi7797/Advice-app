757ed7462e8746db79e94334eec2cae647142c3b
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-04-18 20:41:27 +0530

    commit : data change on button click from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      advise: ""
+    };
+  }
+  // state = {
+  //   advise: ""
+  // };
+
+  componentDidMount() {
+    console.log("compoennet did mount");
+    // let res = axios
+    //   .get("https://api.adviceslip.com/advice")
+    //   .then(resolve => {
+    //     const { advice } = resolve.data.slip;
+    //     console.log(advice);
+
+    //     this.setState({ advice });
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    // console.log(res);
+    this.fetchAdvise();
+  }
+
+  fetchAdvise = () => {
+    let res = axios
+      .get("https://api.adviceslip.com/advice")
+      .then(resolve => {
+        const { advice } = resolve.data.slip;
+        console.log(advice);
+
+        this.setState({ advice });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    console.log(res);
+  };
+  render() {
+    const { advice } = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="btn btn-success" onClick={this.fetchAdvise}>
+            Give me Advice
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
